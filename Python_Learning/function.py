9c64ef29297df32e972bc952240bbf861d4749a4
Abu Siddique <siddique.abu001@gmail.com>
2026-08-12 15:25:32 -0400

    add functions

---
+++ Python_Learning/function.py
@@ -0,0 +1,12 @@
+def say_hi(name):
+    print("Hello, this is {}.".format(name));
+
+say_hi("Abu");
+
+
+
+def say_hi(name = 'Abu'):
+    print("Hello, this is {}.".format(name));
+
+say_hi();
+say_hi("jhon");
