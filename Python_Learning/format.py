29b524c31687efcd3ec06a26b52700bb3716bbba
Abu Siddique <siddique.abu001@gmail.com>
2026-08-12 12:32:46 -0400

    add input

--- Python_Learning/format.py
+++ Python_Learning/format.py
@@ -1,6 +1,7 @@
 
 
 # string formatting
+
 first = 'I';
 second= 'love';
 third = 'apple';
@@ -13,3 +14,9 @@ print('i love python {}'.format(version));
 # "<" left(default) "^"center and ">" right alignment
 
 print('{0:<8} | {1:^8}| {2:>8}'.format(first, second, third));
+
+# f Float and .Nf N= the number of decimal places
+# ex: {:.2f} will round the float to 2 decimal places
+pi = 3.141592653589793238462643383279502884197169
+print('Pi is approximately {:.2f}'.format(pi));
+print('{0:<8} | {1:^8}| {2:>8.2f}'.format(first, second, pi));
---
+++ Python_Learning/input.py
@@ -0,0 +1,3 @@
+#input() accept standard input as string and return it
+name = input("Enter your name: ");
+print("Hello, {}!".format(name));
