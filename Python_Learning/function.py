0784dfb21f3067c645a3fb777a748056decef12f
Abu Siddique <siddique.abu001@gmail.com>
2026-08-12 15:36:35 -0400

    help functions

--- Python_Learning/function.py
+++ Python_Learning/function.py
@@ -4,9 +4,21 @@ def say_hi(name):
 say_hi("Abu");
 
 
-
+#default parameter value
 def say_hi(name = 'Abu'):
     print("Hello, this is {}.".format(name));
 
 say_hi();
 say_hi("jhon");
+
+#default parameter value
+def say_hello(name, age = 20):
+    print("Hello, this is {} {}.".format(name, age));
+say_hello("jhon", 25);
+say_hello("jhon");
+
+#build in help function
+def say_hi(name = 'AAA'):
+    "'This function will print hello message with name'"
+    print("Hello, this is {}.".format(name));
+help(say_hi);
