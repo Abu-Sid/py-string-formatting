f31988521cb1bab6f732f3d9769e565c59459f59
Abu Siddique <siddique.abu001@gmail.com>
2026-08-12 15:49:52 -0400

    nested  functions

--- Python_Learning/function.py
+++ Python_Learning/function.py
@@ -22,3 +22,24 @@ def say_hi(name = 'AAA'):
     "'This function will print hello message with name'"
     print("Hello, this is {}.".format(name));
 help(say_hi);
+
+#nested function
+def get_name():
+    names = input("Enter your name: ");
+    return names;
+
+def say_name(names):
+    print("Hello, this is {}.".format(names));
+
+def get_and_say_name():
+    """This function will get name from user and print hello message with name"""
+    names = get_name();
+    say_name(names);
+get_and_say_name();
+
+
+def outer_function():
+    def inner_function():
+        print("This is an inner function.");
+    inner_function();
+outer_function();
