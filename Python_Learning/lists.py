980aaca47b9fc15804012d5740af6f202d091f2f
Abu Siddique <siddique.abu001@gmail.com>
2026-08-12 16:10:39 -0400

    lists

---
+++ Python_Learning/lists.py
@@ -0,0 +1,20 @@
+# js array is list in python
+animals=['cat', 'dog', 'rabbit', 'parrot', 'fish'];
+print(animals[0]);
+
+animals.append('tiger');
+print(animals);
+animals.remove('fish');
+print(animals);
+animals.insert(1, 'hamster');
+print(animals);
+animals.pop(2);
+print(animals);
+animals.sort();
+print(animals);
+
+animals.reverse();
+print(animals); 
+
+animals.extend(['lion', 'elephant']);
+print(animals);
