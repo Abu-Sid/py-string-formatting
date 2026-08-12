ac1887f263b957c7267511d8a699f9221063af23
Abu Siddique <siddique.abu001@gmail.com>
2026-08-12 13:48:44 -0400

    add conditions

---
+++ Python_Learning/conditions.py
@@ -0,0 +1,10 @@
+age = 55;
+
+if age>= 18:
+    print("You are an adult.");
+elif age>= 13:
+    print("You are a teenager.");
+else:
+    print("You are not a teenager or an adult.");
+
+print("Thank you.");
