498d501d1b086bd85c8fea73f0dfd83550e9ba33
Abu Siddique <siddique.abu001@gmail.com>
2026-08-12 13:19:22 -0400

    add exercise 3

--- Python_Learning/exercise.py
+++ Python_Learning/exercise.py
@@ -12,3 +12,15 @@ print(mineral)
 user_input = input("Please type something and press enter ");
 print('You entered: {}'.format(user_input));
 
+# exercise 3
+
+text = input("what would you name your pet? ");
+text_len = len(text);
+
+print('                {}'.format('_'*text_len));
+print('                |{}|'.format(text));
+print('                {}'.format('-'*text_len));
+print('        /')
+print(' /\_/\ /')
+print('( o.o )')
+print(' > ^ <')
